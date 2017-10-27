090fbf8aa584b6ba81844f2796e0d6239337754a
Biohazard <rowdysayan99@gmail.com>
2017-10-27 21:52:31 +0530

    Update land.mk for PureFusion OS

--- land.mk
+++ land.mk
@@ -26,8 +26,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common CarbonROM stuff.
-$(call inherit-product, vendor/carbon/config/common.mk)
-$(call inherit-product, vendor/carbon/config/gsm.mk)
+$(call inherit-product, vendor/pure/configs/pure_phone.mk)
 
 # Inherit from land device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -35,7 +34,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := land
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := carbon_land
+PRODUCT_NAME := land
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -45,8 +44,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.carbon.maintainer="RiteshSaxena"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=Xiaomi/land/land:6.0.1/MMB29M/V8.5.4.0.MALMIED:user/release-keys \
-    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V8.5.4.0.MALMIED release-keys"
+    BUILD_FINGERPRINT=Xiaomi/land/land:6.0.1/MMB29M/V8.2.5.0.MALMIDL:user/release-keys \
+    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V8.2.5.0.MALMIDL release-keys"
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
     ro.product.model
