5c9e6447fc1612378115e2a54de5689bc88e8121
Biohazard <rowdysayan99@gmail.com>
2017-10-28 10:34:04 +0530

    Update land.mk

--- land.mk
+++ land.mk
@@ -25,7 +25,7 @@ $(call inherit-product, $(LOCAL_PATH)/PlatformConfig.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CarbonROM stuff.
+# Inherit some common PureFusionOs stuff.
 $(call inherit-product, vendor/pure/configs/pure_phone.mk)
 
 # Inherit from land device
@@ -39,9 +39,6 @@ PRODUCT_NAME := land
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := land
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.carbon.maintainer="RiteshSaxena"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=Xiaomi/land/land:6.0.1/MMB29M/V8.2.5.0.MALMIDL:user/release-keys \
